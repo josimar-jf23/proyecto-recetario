d413bdf814f82d54f2c16442b3d74f24f083b2d7
Josimar Jauregui <josimar.jauregui.negocios@gmail.com>
2024-06-22 21:21:46 -0500

    add lista usuarios

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,3 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
 export const getUsers = async (req, res) => {
-    res.json({message:'lista usuarios'});
+    const users = await prisma.user.findMany({
+        select: {
+            id: true,
+            email: true,
+            name: true,
+            roleId: true,
+            role: {
+                select: {
+                    id: true,
+                    name: true,
+                },
+            }
+        },
+    });
+    console.log(users);
+    res.json(users);
 };
